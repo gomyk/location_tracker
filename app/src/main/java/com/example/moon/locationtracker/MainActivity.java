eb9896d6e4307f5c31d4105e77e5b593b2bf7dd0
Moonwon <gomyk@naver.com>
2018-07-09 18:48:32 +0900

    resolve permission problem

--- app/src/main/java/com/example/moon/locationtracker/MainActivity.java
+++ app/src/main/java/com/example/moon/locationtracker/MainActivity.java
@@ -1,45 +1,156 @@
 package com.example.moon.locationtracker;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    TextView tv;
+    ToggleButton tb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        int permissionCheck = ContextCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION);
+        if(permissionCheck != PackageManager.PERMISSION_GRANTED){
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+                // Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
 
-        // 위치기반 어플리케이션 : 사용자의 위치를 활용한 어플리케이션
-        // 폰에서 위치정보를 얻는 방법
-        // 1. GPS - 위성에서 정보를 받아 삼각측량으로 위치를 계산,
-        //           정확하다, 건물 안에서는 안된다
-        // 2. 3G망 - 인접된 전화기지국에서오는 전파의 시간 차이로 위치를 계산,
-        //          실내에서도 가능
-        // 3. WiFi 의 AP
+            } else {
+                // No explanation needed, we can request the permission.
 
-        // ■ 위치정보를 사용하려면, 권한 설정을 해야함 AndroidManifest.xml
-        //           android.permission.ACCESS_FINE_LOCATION
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        200);
 
-        TextView tv = (TextView)findViewById(R.id.textView2);
-
-        LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-        // 위치관리자 객체를 얻어온다
-
-//        lm.getBestProvider(criteria, enabledOnly)
-        List<String> list = lm.getAllProviders(); // 위치제공자 모두 가져오기
-        Toast.makeText(getApplicationContext(),"ListCount : "+list.size(),Toast.LENGTH_LONG).show();
-        String str = ""; // 출력할 문자열
-        for (int i = 0; i < list.size(); i++) {
-            str += "위치제공자 : " + list.get(i) + ", 사용가능여부 -"
-                    + lm.isProviderEnabled(list.get(i)) +"\n";
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
         }
-        tv.setText(str);
+
+        // Location 제공자에서 정보를 얻어오기(GPS)
+        // 1. Location을 사용하기 위한 권한을 얻어와야한다 AndroidManifest.xml
+        //     ACCESS_FINE_LOCATION : NETWORK_PROVIDER, GPS_PROVIDER
+        //     ACCESS_COARSE_LOCATION : NETWORK_PROVIDER
+        // 2. LocationManager 를 통해서 원하는 제공자의 리스너 등록
+        // 3. GPS 는 에뮬레이터에서는 기본적으로 동작하지 않는다
+        // 4. 실내에서는 GPS_PROVIDER 를 요청해도 응답이 없다.  특별한 처리를 안하면 아무리 시간이 지나도
+        //    응답이 없다.
+        //    해결방법은
+        //     ① 타이머를 설정하여 GPS_PROVIDER 에서 일정시간 응답이 없는 경우 NETWORK_PROVIDER로 전환
+        //     ② 혹은, 둘다 한꺼번헤 호출하여 들어오는 값을 사용하는 방식.
+
+        tv = (TextView) findViewById(R.id.textView2);
+        tv.setText("위치정보 미수신중");
+
+        tb = (ToggleButton)findViewById(R.id.toggle1);
+
+        // LocationManager 객체를 얻어온다
+        final LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        tb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+                    if(tb.isChecked()){
+                        tv.setText("수신중..");
+                        // GPS 제공자의 정보가 바뀌면 콜백하도록 리스너 등록하기~!!!
+                        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, // 등록할 위치제공자
+                                1000, // 통지사이의 최소 시간간격 (miliSecond)
+                                1, // 통지사이의 최소 변경거리 (m)
+                                mLocationListener);
+                        lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, // 등록할 위치제공자
+                                1000, // 통지사이의 최소 시간간격 (miliSecond)
+                                1, // 통지사이의 최소 변경거리 (m)
+                                mLocationListener);
+                         lm.requestLocationUpdates(LocationManager.PASSIVE_PROVIDER, // 등록할 위치제공자
+                                1000, // 통지사이의 최소 시간간격 (miliSecond)
+                                1, // 통지사이의 최소 변경거리 (m)
+                                mLocationListener);
+                    }else{
+                        tv.setText("위치정보 미수신중");
+                        lm.removeUpdates(mLocationListener);  //  미수신할때는 반드시 자원해체를 해주어야 한다.
+                    }
+                }catch(SecurityException ex){
+                }
+            }
+        });
     }
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 200: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+    private final LocationListener mLocationListener = new LocationListener() {
+        public void onLocationChanged(Location location) {
+            //여기서 위치값이 갱신되면 이벤트가 발생한다.
+            //값은 Location 형태로 리턴되며 좌표 출력 방법은 다음과 같다.
+
+            Log.d("test", "onLocationChanged, location:" + location);
+            double longitude = location.getLongitude(); //경도
+            double latitude = location.getLatitude();   //위도
+            double altitude = location.getAltitude();   //고도
+            float accuracy = location.getAccuracy();    //정확도
+            String provider = location.getProvider();   //위치제공자
+            //Gps 위치제공자에 의한 위치변화. 오차범위가 좁다.
+            //Network 위치제공자에 의한 위치변화
+            //Network 위치는 Gps에 비해 정확도가 많이 떨어진다.
+            tv.setText("위치정보 : " + provider + "\n위도 : " + longitude + "\n경도 : " + latitude
+                    + "\n고도 : " + altitude + "\n정확도 : "  + accuracy);
+        }
+        public void onProviderDisabled(String provider) {
+            // Disabled시
+            Log.d("test", "onProviderDisabled, provider:" + provider);
+        }
+
+        public void onProviderEnabled(String provider) {
+            // Enabled시
+            Log.d("test", "onProviderEnabled, provider:" + provider);
+        }
+
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            // 변경시
+            Log.d("test", "onStatusChanged, provider:" + provider + ", status:" + status + " ,Bundle:" + extras);
+        }
+    };
 }
